97c86d5d32fa5c482ffcbc13195572a18ddeb388
Derek Mason <3derekmason@gmail.com>
2021-10-30 10:49:09 -0600

    fetching products, currentProduct, currentProductReview

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -6,22 +6,37 @@ import Reviews from './components/Reviews/Reviews.jsx';
 
 const axios = require('axios');
 
-
-
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [currentProduct, setCurrentProduct] = useState([]);
+  const [currentRatings, setCurrentRatings] = useState([]);
 
   useEffect(() => {
-    console.log('here we are');
     axios({
       method: 'GET',
-      url: '/products/'
+      url: '/products/',
     })
-    .then((response) => {
-      console.log(response.data);
-      setProducts(response.data);
-    })
-    .then(console.log('products:', products))
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .then(
+        axios({
+          method: 'GET',
+          url: '/reviews/meta/?product_id=44388',
+        })
+          .then((res) => {
+            setCurrentRatings(res.data);
+          }),
+      )
+      .then(
+        axios({
+          method: 'GET',
+          url: '/products/',
+        })
+          .then((oneMoreRes) => {
+            setCurrentProduct(oneMoreRes.data[0]);
+          }),
+      );
   }, []);
 
   return (
@@ -30,6 +45,22 @@ const App = () => {
       <Overview />
       <Related />
       <Reviews />
+      <div>
+        <form>
+          <button
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              console.log('ALL PRODUCTS:', products);
+              console.log('CURRENT REVIEW DEETS:', currentRatings);
+              console.log('CURRENT PRODUCT DEETS:', currentProduct);
+            }}
+          >
+            Click me to see some sweet, sweet data...
+
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
