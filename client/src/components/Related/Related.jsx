3e09cbabcbff4a26d90096f322e68ed13edd7fc7
Jack Dowling <jackdowling@Jacks-MacBook-Pro.local>
2021-11-01 10:54:40 -0600

    Implemented data Get in related component

--- client/src/components/Related/Related.jsx
+++ client/src/components/Related/Related.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import AppContext from '../../context';
+import RelatedContext from './context';
 
-const Related = () => (
-  <div>
-    <h4>Related Items</h4>
-    <h4>Your Outfit</h4>
-  </div>
-);
+const Related = () => {
+  const [related, setRelated] = useState([]);
+  const [current, setCurrent] = useState([]);
+  const currentProduct = useContext(AppContext);
+  console.log('Current in Related: ', currentProduct);
+  console.log('related: ', related);
 
+  const relatedItems = () => {
+    const productId = currentProduct.currentProduct.id;
+    console.log('id:', productId);
+    axios.get(`/products/${productId}/related`)
+      .then((res) => {
+        console.log('data: ', res.data);
+        setRelated(res.data);
+      });
+  };
+
+  useEffect(() => {
+    relatedItems();
+  }, [currentProduct]);
+
+  return (
+    <div>
+      <RelatedContext.Provider value={{ related }}>
+        <h4>Related Products</h4>
+      </RelatedContext.Provider>
+      <h4>Your Outfit</h4>
+    </div>
+  );
+};
 export default Related;
 
 // Related Items
