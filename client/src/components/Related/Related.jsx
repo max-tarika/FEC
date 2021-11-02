5dc931616dd4c216f7e89febbe90dd2a83c17d98
Jack Dowling <jackdowling@Jacks-MacBook-Pro.local>
2021-11-02 16:04:42 -0600

    Removed comments from Related.jsx

--- client/src/components/Related/Related.jsx
+++ client/src/components/Related/Related.jsx
@@ -7,33 +7,8 @@ import RelatedList from './RelatedList.jsx';
 const Related = () => {
   const currentProduct = useContext(AppContext);
   const [relatedIds, setRelatedID] = useState([]);
-  // const [relatedProducts, setRelatedProducts] = useState([]);
-  // console.log('here we are ', relatedProducts);
-
-  // const relatedItems = () => {
-  //   const productId = currentProduct.currentProduct.id;
-  //   if (currentProduct.currentProduct !== undefined) {
-  //     axios.get(`/products/${productId}/related`)
-  //       .then((res) => {
-  //         setRelatedID(res.data);
-  //       });
-  //   }
-  // };
-
-  // const populateRelatedItems = () => {
-  //   const results = [];
-  //   if (relatedIds.length > 0) {
-  //     for (let i = 0; i < relatedIds.length; i += 1) {
-  //       axios.get(`/products/${relatedIds[i]}/`).then((res) => {
-  //         results.push(res.data);
-  //       });
-  //     }
-  //     setRelatedProducts(results);
-  //   }
-  // };
 
   useEffect(() => {
-    // const results = [];
     if (currentProduct.currentProduct.length === 0) { return; }
     const relatedItems = () => {
       const productId = currentProduct.currentProduct.id;
@@ -41,36 +16,11 @@ const Related = () => {
         axios.get(`/products/${productId}/related`)
           .then((res) => {
             setRelatedID(res.data);
-            // for (let i = 0; i < res.data.length; i += 1) {
-            //   axios.get(`/products/${res.data[i]}/`).then((response) => {
-            //     results.push(response.data);
-            //   });
-            // } setRelatedProducts(results);
           });
       }
     };
     relatedItems();
   }, [currentProduct]);
-
-  // useEffect(() => {
-  //   if (currentProduct.currentProduct.length === 0) { return; }
-  //   const populateRelatedItems = () => {
-  //     const results = [];
-  //     if (relatedIds.length > 0) {
-  //       for (let i = 0; i < relatedIds.length; i += 1) {
-  //         axios.get(`/products/${relatedIds[i]}/`).then((res) => {
-  //           results.push(res.data);
-  //         });
-  //       }
-  //       setRelatedProducts(results);
-  //     }
-  //   };
-  //   populateRelatedItems();
-  // }, [relatedIds]);
-
-  // useEffect(() => {
-  //   console.log('hellloooo ', relatedProducts.length);
-  // }, [relatedProducts]);
 
   return (
     <div>
@@ -89,17 +39,3 @@ const Related = () => {
   );
 };
 export default Related;
-
-// Related Items
-// Horizontal list composed of product cards
-// [P] [P] [P] [P] >
-// Clicking card causes comparison Modal to pop up
-
-// Outfit
-// Horizontal list composed of product cards
-// Add Item button on left
-// [B] [P] [P] [P] >
-
-// Need Product Card component
-// Need list feild
-// Need arrow buttons
