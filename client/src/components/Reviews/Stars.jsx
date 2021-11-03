192fc8f76337fa0dfbb0d4c4116c53bfa03f5f53
Derek Mason <3derekmason@gmail.com>
2021-11-03 12:24:10 -0600

    filled stars with appropriate logic

--- client/src/components/Reviews/Stars.jsx
+++ client/src/components/Reviews/Stars.jsx
@@ -1,37 +1,31 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect } from 'react';
-import star from '../../assets/star.png';
+import starOutline from '../../assets/starOutline.png';
+import starFill from '../../assets/starFill.png';
+
+import ReviewsContext from './reviewsContext.js';
+
+const Star = ({ index, average }) => (
+  <div className="starWrapper">
+    <img src={starOutline} className="outerStar" />
+    <div className="innerStar" style={{ width: average >= index + 1 ? '100%' : `${((average - Math.floor(average))) * 100}%` }}>
+      <img src={starFill} />
+    </div>
+  </div>
+);
 
 const Stars = (props) => {
-  const { average } = props;
-  const style = {
+  const context = ReviewsContext;
 
-  };
+  const { average } = props;
+
+  useEffect(() => {
+    // fillStars();
+  }, [context]);
 
   return (
-    <div id="stars">
-      <ul className="stars">
-        <li style={style}>
-          {' '}
-          <img src={star} />
-        </li>
-        <li style={style}>
-          {' '}
-          <img src={star} />
-        </li>
-        <li style={style}>
-          {' '}
-          <img src={star} />
-        </li>
-        <li style={style}>
-          {' '}
-          <img src={star} />
-        </li>
-        <li style={style}>
-          {' '}
-          <img src={star} />
-        </li>
-      </ul>
+    <div id="allStars">
+      { [...new Array(5)].map((_, i) => <Star index={i} average={average} />)}
     </div>
   );
 };
