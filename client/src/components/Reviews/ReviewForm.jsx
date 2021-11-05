242c1ede0c9d2f9f689ca3435366f414371e14e5
Derek Mason <3derekmason@gmail.com>
2021-11-05 11:51:22 -0600

    styling and adding elements to modal form

--- client/src/components/Reviews/ReviewForm.jsx
+++ client/src/components/Reviews/ReviewForm.jsx
@@ -2,11 +2,121 @@ import React from 'react';
 
 import AppContext from '../../context.js';
 
+import Stars from './Stars.jsx';
+
 const ReviewForm = () => {
   const placeholder = null;
   return (
+
     <div id="addReviewForm">
-      Hello World
+
+      <h1>Write Your Review</h1>
+      <h3>About the [currentProductName]</h3>
+
+      <div className="formElement">
+        <p>Overall Rating:</p>
+        <Stars average={0} />
+      </div>
+      <div className="formElement">
+        <p>Do you reccomend this product?</p>
+        <div>
+          <input type="radio" id="absolutely" name="drone" value="absolutely" />
+          <label htmlFor="absolutely">Yes, absolutely!</label>
+        </div>
+        <div>
+          <input type="radio" id="noWay" name="drone" value="noWay" />
+          <label htmlFor="noWay">No way!</label>
+        </div>
+      </div>
+      <div className="formElement">
+        <p>Product Characteristics: </p>
+        <h3>Comfort:</h3>
+        <span>
+          <input type="radio" id="poor" name="drone" value="poor" />
+          <label htmlFor="poor">Poor</label>
+        </span>
+        <span>
+          <input type="radio" id="fair" name="drone" value="fair" />
+          <label htmlFor="fair">Fair</label>
+        </span>
+        <span>
+          <input type="radio" id="average" name="drone" value="average" />
+          <label htmlFor="average">Average</label>
+        </span>
+        <span>
+          <input type="radio" id="good" name="drone" value="good" />
+          <label htmlFor="good">Good</label>
+        </span>
+        <span>
+          <input type="radio" id="great" name="drone" value="great" />
+          <label htmlFor="great">Great</label>
+        </span>
+
+        <h3>Fit:</h3>
+        <span>
+          <input type="radio" id="poor" name="drone" value="poor" />
+          <label htmlFor="poor">Poor</label>
+        </span>
+        <span>
+          <input type="radio" id="fair" name="drone" value="fair" />
+          <label htmlFor="fair">Fair</label>
+        </span>
+        <span>
+          <input type="radio" id="average" name="drone" value="average" />
+          <label htmlFor="average">Average</label>
+        </span>
+        <span>
+          <input type="radio" id="good" name="drone" value="good" />
+          <label htmlFor="good">Good</label>
+        </span>
+        <span>
+          <input type="radio" id="great" name="drone" value="great" />
+          <label htmlFor="great">Great</label>
+        </span>
+
+        <h3>Length:</h3>
+        <span>
+          <input type="radio" id="poor" name="drone" value="poor" />
+          <label htmlFor="poor">Poor</label>
+        </span>
+        <span>
+          <input type="radio" id="fair" name="drone" value="fair" />
+          <label htmlFor="fair">Fair</label>
+        </span>
+        <span>
+          <input type="radio" id="average" name="drone" value="average" />
+          <label htmlFor="average">Average</label>
+        </span>
+        <span>
+          <input type="radio" id="good" name="drone" value="good" />
+          <label htmlFor="good">Good</label>
+        </span>
+        <span>
+          <input type="radio" id="great" name="drone" value="great" />
+          <label htmlFor="great">Great</label>
+        </span>
+        <h3>Quality:</h3>
+        <span>
+          <input type="radio" id="poor" name="drone" value="poor" />
+          <label htmlFor="poor">Poor</label>
+        </span>
+        <span>
+          <input type="radio" id="fair" name="drone" value="fair" />
+          <label htmlFor="fair">Fair</label>
+        </span>
+        <span>
+          <input type="radio" id="average" name="drone" value="average" />
+          <label htmlFor="average">Average</label>
+        </span>
+        <span>
+          <input type="radio" id="good" name="drone" value="good" />
+          <label htmlFor="good">Good</label>
+        </span>
+        <span>
+          <input type="radio" id="great" name="drone" value="great" />
+          <label htmlFor="great">Great</label>
+        </span>
+      </div>
     </div>
   );
 };
