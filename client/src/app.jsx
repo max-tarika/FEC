13658bea5b10db20b4f49224aa69e70dc104d7b0
Max Tarika <max.tarika@gmail.com>
2021-11-06 11:17:44 -0600

    Removed jack's stupid console logs

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -61,7 +61,6 @@ const App = () => {
         });
     }
   }, [currentProduct]);
-  console.log('max is a fruitbup');
 
   useEffect(() => {
     if (relatedIds.length > 1) {
@@ -85,8 +84,7 @@ const App = () => {
         });
     }
   }, [relatedIds]);
-  console.log('relatedProducts ', relatedProducts);
-  console.log('related styles ', relatedStyles);
+
   if (!currentProduct) {
     return <div id="loadingScreen">Da Island Is LoADing Mon</div>;
   }
