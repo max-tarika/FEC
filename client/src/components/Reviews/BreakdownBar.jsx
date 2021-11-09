9976f620573d8251229b84079678ff83a7cc9335
Derek Mason <3derekmason@gmail.com>
2021-11-09 12:13:50 -0700

    mapped each products characteristics to its own product rating bar

---
+++ client/src/components/Reviews/BreakdownBar.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useContext } from 'react';
+import AppContext from '../../context.js';
+
+const BreakdownBar = (props) => {
+  const { characteristic } = props;
+
+  const context = useContext(AppContext);
+  const { currentProduct } = context;
+  const { characteristics } = context.currentReview;
+
+  const calcOffset = (num) => (num / props.total) * 100;
+
+  useEffect(() => {
+
+  }, [currentProduct]);
+
+  if (!characteristics) return (<h3>Loading...</h3>);
+
+  return (
+    <div id="breakdownBar">
+      <div id={characteristic}>
+        {characteristic}
+        <div className="ReviewBar">
+          <span id="reviewBarFill" style={{ left: `${calcOffset(characteristics[characteristic]?.value)}%` }} />
+        </div>
+        <span id="productBreakdownDesc">
+          <p>Beach Bob</p>
+          <p>-</p>
+          <p>Island Top Shelf</p>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default BreakdownBar;
