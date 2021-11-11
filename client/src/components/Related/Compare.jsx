aef77dc8a5b175ea0e08b345f5afec0d87ff365f
Jack Dowling <jackdowling@Jacks-MacBook-Pro.local>
2021-11-11 12:37:30 -0700

    Oh shit it kinda fuckin works lets goooooo

--- client/src/components/Related/Compare.jsx
+++ client/src/components/Related/Compare.jsx
@@ -7,7 +7,7 @@ const Compare = () => {
   const { currentFeature, relatedProducts } = useContext(AppContext);
   const { showCompare, clickedItem } = useContext(RelatedContext);
   const [comparisonModal, setComparisonModal] = useState([]);
-  const [compareItem, setCompareItem] = useState('');
+  const [compareItem, setCompareItem] = useState();
 
   useEffect(() => {
     for (const entry of relatedProducts) {
@@ -15,31 +15,45 @@ const Compare = () => {
         setCompareItem(entry);
       }
     }
-    const store = [];
-    if (currentFeature && compareItem) {
-      let { features } = currentFeature;
-      for (let i = 0; i < features?.length; i += 1) {
-        const obj = {};
-        obj.a = '√';
-        const key = features[i].feature;
-        obj[key] = features[i].value;
-        obj.b = '';
-        store.push(obj);
-      }
-      features = compareItem;
-      for (let i = 0; i < features?.length; i += 1) {
-        const obj = {};
-        obj.a = '';
-        const key = features[i].feature;
-        obj[key] = features[i].value;
-        obj.b = '√';
-        store.push(obj);
-      }
-    }
-    setComparisonModal(store);
   }, [showCompare]);
 
-  if (showCompare && compareItem) {
+  useEffect(() => {
+    if (compareItem) {
+      const store = [];
+      const currentArray = currentFeature.features;
+      const compareArray = compareItem?.features;
+      let array = currentArray.concat(compareArray);
+      array = [...new Set([...currentArray, ...compareArray])];
+      for (const val of array) {
+        const obj = {};
+        if (val.value === null) {
+          val.value = '';
+        }
+        if (val.feature === null) {
+          val.feature = '';
+        }
+        const phrase = `${val.value} ${val.feature}`;
+        if (currentArray.includes(val) && !compareArray.includes(val)) {
+          obj.a = '√';
+          obj.feat = phrase;
+          obj.b = '';
+        } else if (!currentArray.includes(val) && compareArray.includes(val)) {
+          obj.a = '';
+          obj.feat = phrase;
+          obj.b = '√';
+        } else if (currentArray.includes(val) && compareArray.includes(val)) {
+          obj.a = '√';
+          obj.feat = phrase;
+          obj.b = '√';
+        }
+        store.push(obj);
+      }
+      setComparisonModal(store);
+      console.log(store);
+    }
+  }, [compareItem]);
+
+  if (showCompare && comparisonModal.length) {
     return (
       <div id="comparisonModal">
         <table>
@@ -53,6 +67,15 @@ const Compare = () => {
             <th>With</th>
             <th>{compareItem.name}</th>
           </tr>
+          {comparisonModal.map((item) => (
+            <div>
+              <tr>
+                <td>{item.a}</td>
+                <td>{item.feat}</td>
+                <td>{item.b}</td>
+              </tr>
+            </div>
+          ))}
         </table>
       </div>
     );
